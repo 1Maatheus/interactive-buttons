44083b1870b9fd57f6b70bb61ad4440667e76a8b
Matheus Oliveira Monteiro <matheusoliveira7227@gmail.com>
2023-06-06 19:55:03 -0300

    changed script

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-function mudarCor(cor) {
-  document.getElementById("container").style.background = cor;
+let red = "#FF0000";
+let green = "#00FF00";
+let yellow = "#FFFF00";
+
+function vermelho() {
+  document.getElementById("container").style.background = red;
+}
+
+function verde() {
+  document.getElementById("container").style.background = green;
+}
+
+function amarelo() {
+  document.getElementById("container").style.background = yellow;
 }
 
 function reset(cor) {
